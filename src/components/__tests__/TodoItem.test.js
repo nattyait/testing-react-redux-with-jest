ed379bba13760dc5b227f860812a49b368786ae2
nattyait <nattanicha@gmail.com>
2017-03-20 11:59:33 +0700

    Use jest snapshot test :baby:

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
 import TodoItem from '../TodoItem'
 
 // Describe what to test
@@ -13,9 +14,19 @@ describe('TodoItem', () => {
     expect([1, 2, 3]).toEqual([1, 2, 3])
   })
   describe('TodoItem', () => {
+    it('matches its snapshot - not complete', () => {
+      const component = shallow(<TodoItem text="Not complete" />)
+      expect(toJson(component)).toMatchSnapshot()
+    })
     it('render correct structure', () => {
       const component = shallow(<TodoItem text="Hello" />)
       expect(component.is('li')).toBe(true)
     })
+  })
+  it('has complete class if receving complete prop', () => {
+    const component = shallow(
+      <TodoItem text="Hello" complete />
+    )
+    expect(component.hasClass('complete')).toBe(true)
   })
 })
