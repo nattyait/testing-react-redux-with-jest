bff83f8189970b96f38578e7bc315072c38a55f9
nattyait <nattanicha@gmail.com>
2017-03-20 14:57:13 +0700

    Changes state when typing :baby:

---
+++ src/components/__tests__/NewTodo.test.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import NewTodo from '../NewTodo'
+
+describe('NewTodo', () => {
+  let component
+  let props
+
+  beforeEach(() => {
+    component = shallow(<NewTodo />)
+  })
+
+  it('matches its snapshot', () => {
+    expect(component).toMatchSnapshot()
+  })
+
+  it('renders correct structure', () => {
+    expect(component.is('div')).toBe(true)
+    expect(component.hasClass('new-todo')).toBe(true)
+    expect(component.find('input').length).toBe(1)
+    expect(component.find('button').length).toBe(1)
+    expect(component.find('button').hasClass('btn')).toBe(true)
+    expect(component.find('button').hasClass('btn-primary')).toBe(true)
+  })
+  it('changes state when typing', () => {
+    component.find('input').simulate('change', {
+      target: {
+        value: 'Hello',
+      },
+    })
+    expect(component.state('text')).toBe('Hello')
+  })
+})
