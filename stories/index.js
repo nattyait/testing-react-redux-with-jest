2f0b0981c59f9ab1baf5356dbdfae013a1fb9837
nattyait <nattanicha@gmail.com>
2017-03-20 10:31:24 +0700

    Add TodoItem to storybook and config webpack for storybook so it can work with cssmodules :baby:

--- stories/index.js
+++ stories/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
 import Button from './Button';
 import Welcome from './Welcome';
+import TodoItem from '../src/components/TodoItem';
 
 storiesOf('Welcome', module)
   .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
+    <Welcome showApp={linkTo('Button')} />
   ));
 
 storiesOf('Button', module)
@@ -15,3 +16,11 @@ storiesOf('Button', module)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
   ));
+
+storiesOf('TodoItem', module)
+  .add('not compolete', () => (
+    <TodoItem text="Not complete" />
+  ))
+  .add('Complete', () => (
+    <TodoItem text="Complete" complete />
+  ))
