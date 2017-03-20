6cdbc221196cc864e9ab1479c80e1b5a21e8f8cf
nattyait <nattanicha@gmail.com>
2017-03-20 11:31:09 +0700

    Config jest :baby:

--- src/components/__tests__/TodoItem.test.js
+++ src/components/__tests__/TodoItem.test.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import TodoItem from '../TodoItem'
+
 // Describe what to test
 describe('TodoItem', () => {
   // Test scenario
@@ -5,4 +9,13 @@ describe('TodoItem', () => {
     expect(5).toBe(5)
   })
   // Test scenario
+  it('checks array', () => {
+    expect([1, 2, 3]).toEqual([1, 2, 3])
+  })
+  describe('TodoItem', () => {
+    it('render correct structure', () => {
+      const component = shallow(<TodoItem text="Hello" />)
+      expect(component.is('li')).toBe(true)
+    })
+  })
 })
